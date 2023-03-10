e1a49e5f42483ad5eb280cdb66e46478b9b51280
Luca <117228370+lukita772@users.noreply.github.com>
2023-03-10 12:59:21 -0300

    Create oneWire.h

---
+++ oneWire.h
@@ -0,0 +1,71 @@
+/*
+ * oneWire.h
+ *
+ *  Created on: 13 feb. 2023
+ *      Author: LAGOSTINI
+ */
+
+#ifndef ONEWIRE_ONEWIRE_H_
+#define ONEWIRE_ONEWIRE_H_
+
+#define ONE_WIRE_PORT GPIOA
+#define ONE_WIRE_PIN GPIO_PIN_7//22
+
+
+typedef enum{
+	MASTER_RESET,
+	MASTER_READ_ROM,
+	MASTER_TX_FN_COMMAND,
+	BUSY,
+	READY,
+	PENDING,
+	FINISHED,
+	READING,
+	WRITING,
+} oneWire_status;
+
+typedef enum{
+	SearchRom = 0xF0,
+	ReadRom = 0x33,
+	MatchRom = 0x55,
+	SkipRom = 0xCC,
+	AalarmSearch = 0xEC,
+	ConvertT = 0x44,
+	WriteScratchpad = 0x4E,
+	ReadScratchpad = 0xBE,
+	CopyScratchpad = 0x48,
+	ReadPowerSupply = 0xB4
+} oneWire_instructionSet;
+
+typedef struct{
+	uint8_t FAMILIY_CODE;
+	uint8_t SERIAL_NUMBER[6];
+	uint8_t CRC_BYTE;
+} oneWire_ROM_DATA;
+
+typedef enum{
+	Status_ReadFamilyCode,
+	Status_ReadSerialNumber,
+	Status_ReadCRC
+}oneWire_ReadRom_Status;
+
+//non implemented functions
+void InitializeOneWire();
+
+//private functions
+void resetPulse();
+uint8_t readBit();
+void writeZero();
+void writeOne();
+uint8_t writeInstruction(oneWire_instructionSet instruction);
+uint8_t readInstruction(oneWire_instructionSet instruction);
+uint8_t readFromDevice(oneWire_instructionSet instruction);
+uint8_t readRisingEdge();
+
+//public functions
+oneWire_status TriggerTemperatureConversion();
+int8_t GetTemperature();
+uint8_t GetFamilyCode();
+
+
+#endif /* ONEWIRE_ONEWIRE_H_ */
